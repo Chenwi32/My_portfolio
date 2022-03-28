f2c299cea365b1cc9f078c3aa15df7edf036849a
chenwi32 <chenwieugene.j@gmail.com>
2022-03-28 03:15:56 +0100

    added a file to the resume page and some styles

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,9 +1,17 @@
+import styles from '../styles/resume.module.css'
+
 const resume = () => {
-    return (
-        <div>
-         <h1>Resume</h1>
-        </div>
-    );
+  return (
+    <div className="secondary_container">
+      <h1 className="title">My Resume</h1>
+
+      <iframe
+        src="/files/Chenwi_ICT_Resume.pdf"
+        className={styles.iframe}
+        frameBorder="0"
+      ></iframe>
+    </div>
+  )
 }
 
-export default resume;
+export default resume
