712db93bea465d039b24ac85e86e637cdc62fd54
Chenwi Eugene <chenwieugene.j@gmail.com>
2022-04-26 18:11:52 +0100

    worked on the background

--- components/homePage.jsx
+++ components/homePage.jsx
@@ -8,20 +8,20 @@ const HomePage = () => {
   return (
     <div className="container mx-auto px-4">
       <div className="home__page item-center flex-col text-center lg:-mx-4  lg:space-x-5">
-        <div className="about-image_container flex-shrink-0  lg:px-4">
-          <Image
-            src="/images/profile.png"
-            alt="Profile"
-            /* priority={true} */
-            className={cx(styles.about_image, 'rounded-full')}
-            id="profile"
-            width={250}
-            height={250}
-          />
+        <div className={cx(styles.about_image_container, 'flex-shrink-0  ')}>
+          <div
+           
+            className={styles.image}
+            
+          >
+            
+          </div>
         </div>
 
         <div className=" lg:mt-10   lg:px-4">
-          <h2 className={cx(styles.title, 'text-2xl lg:text-3xl')}>Chenwi Eugene</h2>
+          <h2 className={cx(styles.title, 'text-2xl lg:text-3xl')}>
+            Chenwi Eugene
+          </h2>
           <EmblaCarousel />
         </div>
       </div>
